579d0dfa973cfaa5a41a55cab78e92382011fbe2
bsekisser <squirmyworms@embarqmail.com>
2021-08-23 10:15:22 -0400

    rol/ror c...  carry in... return carry out...
    	modified:   shift_roll.h

--- shift_roll.h
+++ shift_roll.h
@@ -18,10 +18,15 @@ static inline uint32_t _rol(uint32_t data, int shift)
 	return(_lsl(data, shift) | _lsr(data, -shift));
 }
 
-static inline uint32_t _rol_c(uint32_t data, int shift, int carry)
+static inline uint32_t _rol_c(uint32_t data, int shift, int cin, int* cout)
 {
 	int32_t cmask = _lsl(!!carry, shift - 1);
-	return((_rol(data, shift) & ~cmask) | cmask);
+	uint32_t dout = _rol(data, shift);
+
+	if(cout)
+		*cout = !!(dout & cmask);
+
+	return((dout & ~cin) | cin);
 }
 
 static inline uint32_t _ror(uint32_t data, int shift)
@@ -29,8 +34,13 @@ static inline uint32_t _ror(uint32_t data, int shift)
 	return(_lsl(data, -shift) | _lsr(data, shift));
 }
 
-static inline uint32_t _ror_c(uint32_t data, int shift, int carry)
+static inline uint32_t _ror_c(uint32_t data, int shift, int cin, int* cout)
 {
 	int32_t cmask = _lsl(!!carry, (-shift) - 1);
-	return((_ror(data, shift) & ~cmask) | cmask);
+	uint32_t dout = _ror(data, shift);
+
+	if(cout)
+		*cout = !!(dout & cmask);
+
+	return((dout & ~cin) | cin);
 }
