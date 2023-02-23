c4d1646e5a837f32ae111e19e9e868ac55b81159
bsekisser <squirmyworms@embarqmail.com>
2023-02-23 09:08:54 -0500

    fix callback_list for compile
    	modified:   callback_list.c
    	modified:   callback_list.h

--- callback_list.c
+++ callback_list.c
@@ -1,3 +1,12 @@
+#include "callback_list.h"
+
+/* **** */
+
+#include <stddef.h>
+#include <string.h>
+
+/* **** */
+
 void callback_list_init(callback_list_p cbl, int additional)
 {
 	size_t size = sizeof(callback_list_t);
@@ -13,21 +22,21 @@ void callback_list_init(callback_list_p cbl, int additional)
 void callback_list_process(callback_list_p cbl)
 {
 	for(int i = 0; i < cbl->limit; i++) {
-		callback_list_elem_p cbe = &cbl->cbe[i];
+		callback_list_elem_p cble = &cbl->cble[i];
 		
-		if(cbe->fn)
-			cbe->fn(cbe->param);
+		if(cble->fn)
+			cble->fn(cble->param);
 	};
 }
 
 void callback_list_register_callback(callback_list_p cbl, callback_fn fn, void* param)
 {
 	for(int i = 0; i < cbl->limit; i++) {
-		callback_list_elem_p cbe = cbl->cbe[i];
+		callback_list_elem_p cble = &cbl->cble[i];
 
-		if(0 == cbe->fn) {
-			cbe->fn = fn;
-			cbe->param = param;
+		if(0 == cble->fn) {
+			cble->fn = fn;
+			cble->param = param;
 			return;
 		}
 	}
--- callback_list.h
+++ callback_list.h
@@ -11,9 +11,9 @@ typedef struct callback_list_elem_t {
 typedef struct callback_list_t* callback_list_p;
 typedef struct callback_list_t {
 	int limit;
-	callback_list_elem_t cb[16];
+	callback_list_elem_t cble[16];
 }callback_list_t;
 
-void callback_list_init(callback_list_p cbl);
+void callback_list_init(callback_list_p cbl, int additional);
 void callback_list_process(callback_list_p cbl);
 void callback_list_register_callback(callback_list_p cbl, callback_fn fn, void* param);
