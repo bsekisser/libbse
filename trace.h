903c1d7ac42f3b94b73e3d74b84c1389c0272308
bsekisser <squirmyworms@embarqmail.com>
2024-02-07 13:30:54 -0500

    paste va_args in case not used.
    	modified:   trace.h

--- trace.h
+++ trace.h
@@ -10,7 +10,7 @@ typedef struct trace_t {
 #define TRACE(_f, ...) \
     { \
 	trace_t trace; \
-	TRACE_T_TRACE(TRACE_T(), _f, __VA_ARGS__); \
+	TRACE_T_TRACE(TRACE_T(), _f, ##__VA_ARGS__); \
     }
 
 static trace_p _trace_enter(trace_p trace, const char* file, const char* function, const int line)
@@ -26,7 +26,7 @@ static trace_p _trace_enter(trace_p trace, const char* file, const char* functio
     
 #define TRACE_T_TRACE(_trace, _f, ...) \
     { \
-	printf("%s:%s:@%06u: " _f "\n", _trace->file, _trace->function, _trace->line, __VA_ARGS__); \
+	printf("%s:%s:@%06u: " _f "\n", _trace->file, _trace->function, _trace->line, ##__VA_ARGS__); \
     }
 
 #define TRACE_IF_ABORT(_test) \
