887a234f62b2ceb51258baec8d3bbce2669673b2
bsekisser <squirmyworms@embarqmail.com>
2024-08-05 22:43:52 -0400

    dump_hex: use changed to *const buffer vs const type *buffer.
    	modified:   include/dump_hex.h
    	modified:   source/dump_hex.c

--- include/dump_hex.h
+++ include/dump_hex.h
@@ -6,5 +6,5 @@
 
 /* **** */
 
-void dump_hex(const uint8_t* p, const uint32_t ppa, const unsigned count, const unsigned span, const unsigned ascii);
-void dump_hex_line(const uint8_t* p, const uint32_t base, const uint32_t pos, const unsigned count, const unsigned ascii);
+void dump_hex(uint8_t *const p, const uint32_t ppa, const unsigned count, const unsigned span, const unsigned ascii);
+void dump_hex_line(uint8_t *const p, const uint32_t base, const uint32_t pos, const unsigned count, const unsigned ascii);
--- source/dump_hex.c
+++ source/dump_hex.c
@@ -8,13 +8,13 @@
 
 /* **** */
 
-void dump_hex(const uint8_t* p, const uint32_t ppa, const unsigned count, const unsigned span, const unsigned ascii)
+void dump_hex(uint8_t *const p, const uint32_t ppa, const unsigned count, const unsigned span, const unsigned ascii)
 {
 	for(unsigned index = 0; index < count; index += span)
 		dump_hex_line(&p[index], ppa, index, span, ascii);
 }
 
-void dump_hex_line(const uint8_t* p, const uint32_t base, const uint32_t pos, const unsigned count, const unsigned ascii)
+void dump_hex_line(uint8_t *const p, const uint32_t base, const uint32_t pos, const unsigned count, const unsigned ascii)
 {
 	if(base)
 		printf("0x%08x:0x%03x:", base, pos);
