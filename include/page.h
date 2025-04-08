4840c92d9fcd7afe9b13e2b1a32cc7f4ea271e33
bsekisser <bsekisser@gmail.com>
2025-04-08 16:42:05 -0400

    [PAGE_OFFSET/PTD/PTE]_MASK
    	modified:   include/page.h

--- include/page.h
+++ include/page.h
@@ -2,24 +2,27 @@
 
 #define PAGE(_x)		((_x) >> PAGE_SHIFT)
 #define PAGES(_x)		(PAGE_ALIGN(_x) >> PAGE_SHIFT)
-#define PAGE_ALIGN(_x)	(((_x) + PAGE_SIZE - 1) & PAGE_MASK)
-#define PAGE_MASK		(~(PAGE_SIZE - 1))
-#define PAGE_OFFSET(_x) ((_x) & (PAGE_SIZE - 1))
+#define PAGE_ALIGN(_x)	(((_x) + PAGE_OFFSET_MASK) & PAGE_MASK)
+#define PAGE_MASK		(~PAGE_OFFSET_MASK)
+#define PAGE_OFFSET(_x) ((_x) & PAGE_OFFSET_MASK)
+#define PAGE_OFFSET_MASK (PAGE_SIZE - 1)
 #define PAGE_SHIFT		12
 #define PAGE_SIZE		(1U << PAGE_SHIFT)
 
-#define PTD(_x) ((PAGE(_x) >> PTE_BITS) & (PTD_SIZE - 1))
+#define PTD(_x) ((PAGE(_x) >> PTE_BITS) & PTD_MASK)
 
 #ifndef PTD_BITS
 	#define PTD_BITS 10
 #endif
 
+#define PTD_MASK (PTD_SIZE - 1)
 #define PTD_SIZE (1U << PTD_BITS)
 
-#define PTE(_x) (PAGE(_x) & (PTE_SIZE - 1))
+#define PTE(_x) (PAGE(_x) & PTE_MASK)
 
 #ifndef PTE_BITS
 	#define PTE_BITS 10
 #endif
 
+#define PTE_MASK (PTE_SIZE - 1)
 #define PTE_SIZE (1U << PTE_BITS)
