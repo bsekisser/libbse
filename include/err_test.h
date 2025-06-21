bfd07ca12c0d835f5cdcd93b2e029cd9288bada9
bsekisser <bsekisser@gmail.com>
2025-06-21 10:01:21 -0400

    stderr_log/err_log
    stderr_log uses old behavior of err_log, err_log made generic

--- include/err_test.h
+++ include/err_test.h
@@ -15,11 +15,10 @@
 
 #define ERR_LOG(_f, ...) \
 	{ \
-		LOG("%s:: %s:: %04u:: %s::" _f, \
+		LOG("%s:: %s:: %04u::" _f, \
 			__FILE__, \
 			__func__, \
-			__LINE__, \
-			strerror(errno), ##__VA_ARGS__); \
+			__LINE__, ##__VA_ARGS__); \
 	}
 
 #define _ERR_TEST_ABORT(_test) \
@@ -33,6 +32,27 @@
 #define ERR(_test) _ERR_TEST_ABORT(-1 == (_test))
 #define ERR_IF(_test) _ERR_TEST_ABORT(_test)
 #define ERR_NULL(_test) _ERR_TEST_ABORT(0 == (_test))
+
+#define STDERR_LOG(_f, ...) \
+	{ \
+		LOG("%s:: %s:: %04u:: %s::" _f, \
+			__FILE__, \
+			__func__, \
+			__LINE__, \
+			strerror(errno), ##__VA_ARGS__); \
+	}
+
+#define _STDERR_TEST_ABORT(_test) \
+	{ \
+		if((_test)) { \
+			STDERR_LOG("%s", #_test); \
+			abort(); \
+		} \
+	}
+
+#define STDERR(_test) _STDERR_TEST_ABORT(-1 == (_test))
+#define STDERR_IF(_test) _STDERR_TEST_ABORT(_test)
+#define STDERR_NULL(_test) _STDERR_TEST_ABORT(0 == (_test))
 
 #define assert_abort_msg(_test, _msg, ...) \
 	{ \
